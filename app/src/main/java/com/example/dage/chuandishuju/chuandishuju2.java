a1430225d1576d8b4f86ec186aa068e6a79b95d7
799705398 <799705398@qq.com>
2017-09-12 13:45:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dage/chuandishuju/chuandishuju2.java
@@ -0,0 +1,39 @@
+package com.example.dage.chuandishuju;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class chuandishuju2 extends AppCompatActivity {
+    TextView editText2;
+    EditText editText;
+    Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chuandishuju2);
+        editText2=(TextView) findViewById(R.id.textView4);
+        intent=getIntent();
+        editText=(EditText)findViewById(R.id.editText2);
+        editText2.setText(intent.getStringExtra("sendto2"));
+        Button bt1=(Button)findViewById(R.id.bt2);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent.putExtra("sendto1",editText.getText().toString());
+                setResult(0,intent);
+                finish();
+            }
+        });
+
+    }
+
+
+
+}
